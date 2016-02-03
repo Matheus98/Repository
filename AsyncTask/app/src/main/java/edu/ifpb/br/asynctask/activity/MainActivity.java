dd3199f0432759f96ec73926c171d0a8796e442d
Matheus98 <matheus.costa.rodrigues98@gmail.com>
2016-02-02 22:43:50 -0200

    2nd Commit
    alterações

--- AsyncTask/app/src/main/java/edu/ifpb/br/asynctask/activity/MainActivity.java
+++ AsyncTask/app/src/main/java/edu/ifpb/br/asynctask/activity/MainActivity.java
@@ -3,15 +3,22 @@ package edu.ifpb.br.asynctask.activity;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import edu.ifpb.br.asynctask.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
